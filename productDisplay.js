000cf0553ef95f6d76ec0746b6ec17fa75e677ea
Abdul Zainul Khan <zainulavdeenkhan@outlook.com>
2024-11-10 20:03:46 +0530

    back to version 1.0v

--- productDisplay.js
+++ productDisplay.js
@@ -10,9 +10,7 @@ class productDisplay {
    * First to execute and loads the data from excell sheet
    */
   run() {
-    let file = new fetchExcell(
-      "https://my.microsoftpersonalcontent.com/personal/6f6dbe47ed41720e/_layouts/15/download.aspx?UniqueId=7fe63a90-4a6a-49cd-8a0d-7559b953fecf&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJmYzBlZmYzOS05NGQyLTQxZDUtODM0NS0wOGU5YjMyYTRmYWUiLCJhcHBfZGlzcGxheW5hbWUiOiJHcmFwaCIsImFwcGlkIjoiZGU4YmM4YjUtZDlmOS00OGIxLWE4YWQtYjc0OGRhNzI1MDY0IiwiYXVkIjoiMDAwMDAwMDMtMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL215Lm1pY3Jvc29mdHBlcnNvbmFsY29udGVudC5jb21AOTE4ODA0MGQtNmM2Ny00YzViLWIxMTItMzZhMzA0YjY2ZGFkIiwiZXhwIjoiMTczMTIzNjM5MiJ9.HGEK0hbZdt6tCqFgmKOjT91yx6TSyj8xLbmFfZ3XVw9_57NrcbjaMxnZRjco3v0LHRBdLPbnH7mxjOejtjcLRvgJRvyLbcbx0fP-jV6KnYL9Df2YeiMdySARCJtPnP1jo_JTlBjpcOKZpJJ9qUbUkgZHcooO88BcP-d5hAg3v3omi3tkffjeIkQx9wYJVmYA9oxTRMcsMzN2eOVu9nlhksBtvIDycRl4l55gzI2OrF8sCkbBBlQegxc6jmP7rHGnOCCUQVgFQNJyukYNOdgsc-OgGzUE4TF_GT710TanOFAq15SGHLqkSgKKg6rqzFLnUwP-aJJNDi2QHRMd8pfXip7VSQTWOGNut5iQRCvEqKxFaBXZ_5p7lTJtlnhniWIhRPZNkGkR0P13LWi6SJcZUzlQ94hTTH5MFHNayKyS1cjrDFId2LYeVKSXPaEIF5VMP-hE4iYI1u_kYncGKS46Pg.GnoMqJ_51M9JoIvBqT7umDNzP4WFlWlWRkRBO32zxDg&ApiVersion=2.0"
-    );
+    let file = new fetchExcell(".\\catelog.xlsx");
 
     (async () => {
       try {
